df00da02a2828712372bf56ec966d4917843a0a1
Gurpinder Singh <30500471+guruvirk@users.noreply.github.com>
2024-11-28 16:24:56 +0530

    Added minus numbers logic

--- calc.js
+++ calc.js
@@ -1,8 +1,10 @@
 // var arrStr = '1,5';
 // var arrStr = '1\n2,3';
-var arrStr = '//;\n1;2';
+// var arrStr = '//;\n1;2';
+var arrStr = '1,-2';
 var numberArray = [];
 var lastNum = null;
+var lastNegative = false;
 
 function is_numeric(str) {
     return /^\d+$/.test(str);
@@ -11,18 +13,30 @@ function is_numeric(str) {
 function add(numbers) {
     for (let i = 0; i < numbers.length; i++) {
         if (is_numeric(numbers[i])) {
-            if (lastNum) {
-                lastNum = lastNum + numbers[i];
+            if (lastNegative == true) {
+                if (lastNum) {
+                    lastNum = lastNum + numbers[i];
+                } else {
+                    lastNum = '-' + numbers[i];
+                }
             } else {
-                lastNum = numbers[i];
+                if (lastNum) {
+                    lastNum = lastNum + numbers[i];
+                } else {
+                    lastNum = numbers[i];
+                }
             }
         } else if (numbers[i] == '-') {
+            lastNegative = true;
             if (lastNum) {
                 numbers.push(Number(lastNum));
                 lastNum = null;
             }
         } else {
             if (lastNum) {
+                if (lastNum[0] == '-') {
+                    throw new Error("negative numbers not allowed " + lastNum);
+                }
                 numberArray.push(Number(lastNum));
                 lastNum = null;
             }
@@ -30,6 +44,9 @@ function add(numbers) {
     }
 
     if (lastNum) {
+        if (lastNum[0] == '-') {
+            throw new Error("negative numbers not allowed " + lastNum);
+        }
         numberArray.push(Number(lastNum));
         lastNum = null;
     }
